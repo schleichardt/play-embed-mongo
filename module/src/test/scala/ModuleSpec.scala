2f3d95cabd3e2066807a23efa022ceab8d56afa4
Michael Schleichardt <michael@schleichardt.net>
2013-05-07 21:52:30 +0300

    set tests to pass to test continuous integration server

--- module/src/test/scala/ModuleSpec.scala
+++ module/src/test/scala/ModuleSpec.scala
@@ -3,8 +3,8 @@ import org.specs2.mutable._
 class ModuleSpec extends Specification {
 
   "The ModuleSpec" should {
-    "fail to test the ci server" in {
-      true === false
+    "pass" in {
+      true === true
     }
   }
 }
