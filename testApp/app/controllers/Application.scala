d3660222895c2dc27ac78acc71c67c2863cf5ff6
Michael Schleichardt <michael@schleichardt.net>
2013-05-08 09:04:25 +0200

    added logic to test if it works in dev mode

--- testApp/app/controllers/Application.scala
+++ testApp/app/controllers/Application.scala
@@ -2,11 +2,24 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import com.mongodb.{MongoClient, BasicDBObject}
 
 object Application extends Controller {
   
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def index = Action { implicit request =>
+    val updateDoc = new BasicDBObject().append("$inc", new BasicDBObject().append("visits", 1));
+    val client = new MongoClient("localhost", Play.current.configuration.getInt("embed.mongo.port").get)
+    val collection = client.getDB("test").getCollection("visits")
+    val startPageQuery = new BasicDBObject().append("_id", "startpage")
+    try {
+      collection.update(startPageQuery, updateDoc, true, false)
+      val statsDoc = collection.findOne(startPageQuery)
+      val visits = statsDoc.get("visits")
+      Ok(views.html.index("The page is viewed " + visits + " times."))
+    } catch {
+      case e: com.mongodb.MongoException => BadRequest("Please start with `sbt -Dembed.mongo.enabled=true ~run` because embed mongo is not enabled by default in this project.")
+    }
+
   }
   
 }
